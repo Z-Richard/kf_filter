329b5100775d60ef48fbbc6a3c6434497e2145fe
Richard Zhuang <zhuanghaoyurichard@gmail.com>
2025-04-15 14:12:25 -0700

    0.0.2: refactor & add kf_mask, wave_mask

--- kf_filter/kf_filter.py
+++ kf_filter/kf_filter.py
@@ -16,71 +16,18 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from kf_filter.consts import *
+from kf_filter.consts import (
+    wave_title
+)
+
+from kf_filter.util import (
+    split_hann_taper,
+    _combine_plus_minus,
+    kf_mask,
+    wave_mask
+)
 
 sns.set_theme(style="ticks")
-
-
-def harmonic_func(n, period=365, num_fs=4):
-    """
-    Construct a harmonic function for regression.
-
-    Parameters
-    ----------
-    n : int
-        The sampling dimension obtained from the original data.
-
-    period : float
-        The period of the regression function.
-
-    num_fs : int
-        The number of frequency bands to use.
-
-    Returns
-    -------
-    func : np.ndarray
-        The matrix to regress on to the original timeseries.
-    """
-    func = np.zeros((num_fs*2+1, n), dtype=float)
-    time = np.arange(0, n) * 2 * np.pi / period
-    func[0, :] = np.ones(n)
-    for i in range(num_fs):
-        func[2*i+1, :] = np.sin(i * time)
-        func[(i+1)*2, :] = np.cos(i * time)
-    return func
-
-
-def split_hann_taper(series_length, fraction):
-    """
-    Parameters
-    ----------
-    series_length : int
-        The length of an array-like object.
-
-    fraction : float
-        The fraction of data points to be tapered off at each end
-        of the array.
-
-    Returns
-    -------
-    taper_weights : np.array
-        A series of weights of length `series_length`.
-
-    Implements `split cosine bell` taper of length `series_length`
-    where only fraction of points are tapered (combined on both ends).
-
-    Notes
-    -----
-    This returns a function that tapers to zero on the ends. 
-    To taper to the mean of a series X:
-    XTAPER = (X - X.mean())*series_taper + X.mean()
-    """
-    npts = int(np.rint(fraction * series_length))  # total size of taper
-    taper = np.hanning(npts)
-    series_taper = np.ones(series_length)
-    series_taper[0 : npts // 2 + 1] = taper[0 : npts // 2 + 1]
-    series_taper[-npts // 2 + 1 :] = taper[npts // 2 + 1 :]
-    return series_taper
 
 
 class KF():
@@ -96,31 +43,6 @@ class KF():
     1. https://github.com/tmiyachi/mcclimate/blob/master/kf_filter.py
     2. https://github.com/brianpm/wavenumber_frequency
     """
-    # NOTE: Nondimensionalized dispersion relations for equatorial waves
-    kelvin = lambda op, omega, k, n : op(omega, k)
-    er = lambda op, omega, k, n : op(omega, -k / (2 * n + 1 + k ** 2))
-    eig = lambda op, omega, k, n : op(omega ** 2, k * omega + 1)
-    ig = lambda op, omega, k, n : op(omega ** 2, k ** 2 + (2 * n + 1))
-
-    wave_func = {
-        'kelvin': kelvin,
-        'er': er,
-        'eig': eig,
-        'mrg': eig,  # same as eig...
-        'ig': ig
-    }
-
-    wave_title = {
-        'kelvin': 'Kelvin wave',
-        'er': 'Equatorial Rossby wave',
-        'eig': 'Eastward inertia-gravity wave',
-        'mrg': 'Mixed-Rossby gravity wave',
-        'ig': 'Westward inertia-gravity wave',
-        'td': 'Tropical depression',
-        'mjo': 'Madden-Julian Oscillation'
-    }
-
-    wave_types = list(wave_func.keys())
 
     def __init__(self, 
                  data : xr.DataArray, 
@@ -223,39 +145,8 @@ class KF():
                     }
         )
 
-        # effectively multiply wavenumber by -1 to flip the sign
+        # multiply wavenumber by -1 to flip the sign
         self.kf_reordered = kf_shifted.assign_coords(wavenumber=-1 * kf_shifted['wavenumber']).sortby('wavenumber')
-
-    def _nondim_k_omega(self, h : float) -> tuple[xr.DataArray, xr.DataArray]:
-        r"""Non-dimensionalize k and \omega based on the equivalent depth.
-
-        Parameters
-        ----------
-        h : float
-            Equivalent depth in meters.
-
-        Returns
-        k_nondim, omega_nondim : tuple[xr.DataArray, xr.DataArray]
-
-        Notes
-        -----
-        c = sqrt(g * h)
-        """
-        # expect kf_reordered to exist
-        kf_reordered = self.kf_reordered
-
-        c = np.sqrt(g * h)
-
-        # First convert to wavenumber per Earth radius (m^1)
-        # before we nondimensionalize it according to Vallis (2012)
-        k_nondim = kf_reordered.wavenumber / radius_earth * np.sqrt(c / beta)
-
-        # Here we convert linear frequency from np.fftfreq to 
-        # angular frequency
-        omega_nondim = kf_reordered.frequency * 2 * np.pi / (24 * 3600) / np.sqrt(beta * c)
-
-        # return nondimensionalized k, \omega
-        return k_nondim, omega_nondim
 
     def _preprocess(self, da : xr.DataArray) -> None:
         """
@@ -293,76 +184,6 @@ class KF():
         TODO
         """
         pass
-
-    def kf_mask(self, 
-                fmin: float | None = None, 
-                fmax: float | None = None, 
-                kmin: int | None = None, 
-                kmax: int | None = None,
-                return_individual: bool = False) -> xr.DataArray | tuple[list, list]:
-        r"""
-        A wavenumber-frequency filter for a combination of min/max frequency
-        and min/max wavenumber.
-
-        Parameters
-        ----------
-        fmin, fmax : float or None
-            Minimum and maximum frequency for filtering
-
-        kmin, kmax : int or None
-            Minimum and maximum frequency for filtering
-
-        return_individual : bool
-            Whether or not to return logical_plus and logical_minus separately.
-
-        Returns
-        -------
-        mask : np.array
-
-        Notes
-        -----
-        In order to have the right results, *I think* we need to select
-        frequency with [-fmax, -fmin] & [-kmax, -kmin] \union 
-        [fmin, fmax] & [kmin, kmax].
-        """
-        kf_reordered = self.kf_reordered
-        frequency, wavenumber = kf_reordered.frequency, kf_reordered.wavenumber
-
-        # do separately for positive and negative omega
-        logical_plus = [(frequency > 0)]  # bounding box for positive omega
-        logical_minus = [(frequency < 0)]  # bounding box for negative omega
-
-        # need to do separately for positive frequency and negative frequency
-        if fmin is not None:
-            assert fmin > 0, 'Frequency "fmin" must be greater than 0.'
-            logical_plus.append((frequency > fmin))
-            logical_minus.append((frequency < -fmin))
-
-        if fmax is not None:
-            assert fmax > 0, 'Frequency "fmax" must be greater than 0.'
-            logical_plus.append((frequency < fmax))
-            logical_minus.append((frequency > -fmax))
-
-        if kmin is not None:
-            logical_plus.append((wavenumber > kmin))
-            logical_minus.append((wavenumber < -kmin))
-
-        if kmax is not None:
-            logical_plus.append((wavenumber < kmax))
-            logical_minus.append((wavenumber > -kmax))
-
-        # Check if fmin and fmax are provided
-        if (fmin is not None) and (fmax is not None):
-            assert fmin < fmax, '"fmin" should be smaller than "fmax".'
-
-        if (kmin is not None) and (kmax is not None):
-            assert kmin < kmax, 'Wavenumber "kmin" should be smaller than "kmax".'
-
-        # Filter both positive and negative frequencies (plus and minus)
-        if return_individual:
-            return logical_plus, logical_minus
-        else:
-            return self._combine_plus_minus(logical_plus, logical_minus)
 
     def kf_filter(self, mask : xr.DataArray) -> xr.DataArray:
         """
@@ -414,30 +235,6 @@ class KF():
 
         return self.data_filtered
     
-    @staticmethod
-    def _combine_plus_minus(logical_plus : list, 
-                            logical_minus : list) -> xr.DataArray:
-        """
-        Combine masks from positive frequency domain
-        with that from negative frequency domain.
-
-        Parameters
-        ----------
-        logical_plus : list
-            List of logical conditions for positive frequencies.
-        logical_minus : list
-            List of logical conditions for negative frequencies.
-
-        Returns
-        -------
-        mask : xr.DataArray
-            Combined mask.
-        """
-        omega_plus = reduce(lambda x, y: xr.apply_ufunc(xr.ufuncs.logical_and, x, y), logical_plus)
-        omega_minus = reduce(lambda x, y: xr.apply_ufunc(xr.ufuncs.logical_and, x, y), logical_minus)
-
-        return xr.ufuncs.logical_or(omega_plus, omega_minus)
-    
     def _save_mask(self, 
                    mask : xr.DataArray, 
                    wave_type : str) -> None:
@@ -488,36 +285,10 @@ class KF():
         For TD-type wave, we do not attempt to nondimensionalize
         omega and k. 
         """
-        logical_plus, logical_minus = self.kf_mask(fmin, fmax, kmin, kmax, return_individual=True)
-
-        if wave_type not in KF.wave_types:
-            raise ValueError(f'Unsupported wave_type "{wave_type}".')
-
-        # Select the dispersion relation function from class attributes
-        func = KF.wave_func[wave_type]
-        
-        if hmin is not None:
-            k, omega = self._nondim_k_omega(hmin)
-            logical_plus.append(func(operator.gt, omega, k, n))
-
-            # For IG, EIG, MRG, need to use gt because of Omega^2
-            if wave_type in ['ig', 'eig', 'mrg']:
-                logical_minus.append(func(operator.gt, omega, k, n))
-            # For ER and KW, use lt because of Omega
-            else:
-                logical_minus.append(func(operator.lt, omega, k, n))
-
-        if hmax is not None:
-            k, omega = self._nondim_k_omega(hmax)
-            logical_plus.append(func(operator.lt, omega, k, n))
-
-            if wave_type in ['ig', 'eig', 'mrg']:
-                logical_minus.append(func(operator.lt, omega, k, n))
-            # For ER and KW, use gt because of Omega
-            else:
-                logical_minus.append(func(operator.gt, omega, k, n))
-
-        mask = self._combine_plus_minus(logical_plus, logical_minus)
+        mask = wave_mask(self.kf_reordered.wavenumber,
+                         self.kf_reordered.frequency,
+                         wave_type,
+                         fmin, fmax, kmin, kmax, hmin, hmax, n)
         self._save_mask(mask, wave_type)
 
         return self.kf_filter(mask)
@@ -688,11 +459,13 @@ class KF():
         kmin, kmax : int or None
             Minimum and maximum wavenumber.
         """
-        logical_plus, logical_minus = self.kf_mask(fmin, fmax, 
-                                                   kmin, kmax,
-                                                   return_individual=True)
-
         wavenumber, frequency = self.kf_reordered.wavenumber, self.kf_reordered.frequency
+
+        logical_plus, logical_minus = kf_mask(wavenumber,
+                                              frequency,
+                                              fmin, fmax, 
+                                              kmin, kmax,
+                                              return_individual=True)
 
         logical_plus.append((84 * frequency + wavenumber - 22 < 0))
         logical_minus.append((84 * frequency + wavenumber + 22 > 0))
@@ -700,7 +473,7 @@ class KF():
         logical_plus.append((210 * frequency + 2.5 * wavenumber - 13 > 0))
         logical_minus.append((210 * frequency + 2.5 * wavenumber + 13 < 0))
 
-        mask = self._combine_plus_minus(logical_plus, logical_minus)
+        mask = _combine_plus_minus(logical_plus, logical_minus)
 
         self._save_mask(mask, 'td')
         return self.kf_filter(mask)
@@ -727,9 +500,11 @@ class KF():
         and a band of frequencies between 1/96 and 1/20 cycles per day
         as in Mayta and Adames (2023).
         """
-        kf_mask = self.kf_mask(fmin, fmax, kmin, kmax)
-        self._save_mask(kf_mask, 'mjo')
-        return self.kf_filter(kf_mask)
+        mask = kf_mask(self.kf_reordered.wavenumber,
+                          self.kf_reordered.frequency,
+                          fmin, fmax, kmin, kmax)
+        self._save_mask(mask, 'mjo')
+        return self.kf_filter(mask)
 
     def _visualize_aux(self, 
                        wave_type : str, 
@@ -746,7 +521,7 @@ class KF():
         if hide_negative:
             plt.ylim([0, self.kf_reordered.frequency.max()])
         # plt.grid()
-        plt.title(f'{KF.wave_title[wave_type]} Filter')
+        plt.title(f'{wave_title[wave_type]} Filter')
         plt.show()
 
     def get_mask(self, wave_type):
